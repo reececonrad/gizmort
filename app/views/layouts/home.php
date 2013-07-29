8e3bb9c19fba060bb7b7cef57fa351664ec9c6ef
Reece Conrad <reece.conrad@gmail.com>
2013-07-28 19:50:44 -0500

    Added twitter bootstrap

---
+++ app/views/layouts/home.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+	<title></title>
+	<link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+	 <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  	<link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+  	<link rel="stylesheet" type="text/css" href="/stylesheets/style.css" />
+  	<!--[if IE]>
+      <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  	<![endif]-->
+</head>
+
+
+
+
+
+
+
+
+
+	<script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+ </body>
+</html>
